d1c251a73372e2bfc4e0f48a23ca31c9708b8a23
Pranshu Dobhal <pranshu.dobhal@gmail.com>
2021-01-06 00:00:41 +0530

    fix: fixed issue with check triangle and added validations in input

--- app.js
+++ app.js
@@ -8,32 +8,38 @@ document.getElementById('triangle-or-not').addEventListener('click', function (e
   document.getElementById('find-angle').classList.add('hidden');
   document.getElementById('find-hypotenuse').classList.add('hidden');
   document.getElementById('output-sum').innerText = '';
+});
 
-  //Ask 3 angles from user
-  var angleOne = document.getElementById('ang1');
-  var angleTwo = document.getElementById('ang2');
-  var angleThree = document.getElementById('ang3');
+//Ask 3 angles from user
+var angleOne = document.getElementById('ang1');
+var angleTwo = document.getElementById('ang2');
+var angleThree = document.getElementById('ang3');
 
-  document.getElementById('submit-sum').addEventListener('click', sumOfTriangles);
+document.getElementById('submit-sum').addEventListener('click', sumOfTriangles);
 
-  //add the values provided by user and check if triangle or not
-  function sumOfTriangles() {
-    var angleOneValue = angleOne.value;
-    var angleTwoValue = angleTwo.value;
-    var angleThreeValue = angleThree.value;
+//add the values provided by user and check if triangle or not
+function sumOfTriangles() {
+  var angleOneValue = angleOne.value;
+  var angleTwoValue = angleTwo.value;
+  var angleThreeValue = angleThree.value;
 
+  if (angleOneValue == '' || angleTwoValue == '' || angleThreeValue == '') {
+    document.getElementById('output-sum').innerText = 'Please provide all three angles!';
+  } else if (angleOneValue == 0 || angleTwoValue == 0 || angleThreeValue == 0) {
+    document.getElementById('output-sum').innerText = 'Angle cannot be ZERO! Please try again!';
+  } else {
     var sum = Number(angleOneValue) + Number(angleTwoValue) + Number(angleThreeValue);
-
     if (sum === 180) {
       document.getElementById('output-sum').innerText = 'Yayy! It is a triangle 😀';
     } else {
       document.getElementById('output-sum').innerText = "🤨 The angles don't add up! Try Again!";
     }
-    angleOne.value = '';
-    angleTwo.value = '';
-    angleThree.value = '';
   }
-});
+
+  angleOne.value = '';
+  angleTwo.value = '';
+  angleThree.value = '';
+}
 /** End triangle or not */
 
 /** Start type of triangle */
